c5c4ca7cde25af631ce2731b34c7a4627d747494
andreacardenas0610 <andrea.cardenas0610@gmail.com>
2026-04-07 13:24:55 -0500

    Mi primera API

---
+++ route/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require("../Config/db");
+const userroute = require('./user.route');
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userroute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
